b54bfd1d720ce6e44ca10506f70c0368c542fef4
FelixCukerman <andreyka.abiev@gmail.com>
2018-07-09 01:28:34 +0300

    last push

---
+++ WebApplication1/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class TodoController : Controller
+    {
+        private TodoService todoService;
+        public TodoController()
+        {
+            todoService = new TodoService();
+        }
+        public IActionResult Index()
+        {
+            var todos = todoService.GetTodos();
+            return View(todos);
+        }
+        public IActionResult TodoID(int id)
+        {
+            var todo = todoService.GetTodos().Where(x => x.Id == id).FirstOrDefault();
+            return View(todo);
+        }
+    }
+}
